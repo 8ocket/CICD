3cd344101fb92b85e73388b3e31ab7490d84fb62
hoombahh <hanwoolmeroomar@gmail.com>
2026-04-06 14:39:08 +0900

    feat: complete infra automation with generic templates and deploy script

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,7 @@ echo "   - Redis: $REDIS_ENDPOINT"
 echo "   - BE API: $BE_API_URL"
 echo "------------------------------------------"
 
-# 2. Kubernetes Secret 
-# (시크릿 키 이름들을 YAML에서 참조하는 이름과 일치시켰습니다)
+# 2. BE secret 
 kubectl delete secret mindlog-be-secret -n default --ignore-not-found
 kubectl create secret generic mindlog-be-secret -n default \
   --from-literal=DB_USERNAME="mindlog" \
@@ -32,39 +31,39 @@ kubectl create secret generic mindlog-be-secret -n default \
   --from-literal=DB_NAME='mindlog_db' \
   --from-literal=KT_JWT_SECRET='kt-cloud-8ocket-mindLog-jwt-secret-key' \
   --from-literal=DB_DEV_HOST="$RDS_ENDPOINT" \
-  --from-literal=REDIS_DEV_HOST="$REDIS_ENDPOINT"
+  --from-literal=REDIS_DEV_HOST="$REDIS_ENDPOINT" \
+  --from-literal=REDIS_PASSWORD=''
 
 
-# be sync
+# 3. BE sync
 if [ -f "./backend/deployment.yaml" ]; then
-
-    # 1. 이미지 주소 치환
-    sed -i 's|${ECR_IMAGE_URL}|'"$ECR_IMAGE_URL"'|g' ./backend/deployment.yaml
-
-    # 2. RDS 엔드포인트 치환
-    sed -i 's|${RDS_ENDPOINT}|'"$RDS_ENDPOINT"'|g' ./backend/deployment.yaml
-    sed -i 's|/mindlog_db"|/mindlog_db?sslmode=require"|g' ./backend/deployment.yaml
+    sed -i "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" ./backend/deployment.yaml
     sed -i "s|\${RDS_ENDPOINT}|$RDS_ENDPOINT|g" ./backend/deployment.yaml
     sed -i "s|\${REDIS_ENDPOINT}|$REDIS_ENDPOINT|g" ./backend/deployment.yaml
-    echo ">>> Check replacement result:"
-    grep -A 1 "image:" ./backend/deployment.yaml
-    grep -A 1 "DB_DEV_HOST" ./backend/deployment.yaml
-    grep "SPRING_DATASOURCE_URL" -A 1 ./backend/deployment.yaml
-
 else
-    echo "Error: ./backend/deployment.yaml not found!"
+    echo "Error: no ./backend/deployment.yaml "
     exit 1
 fi
 
-# fe sync
+# 4. AI sync
+if [ -f "./ai/deployment.yaml" ]; then
+    sed -i "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" ./ai/deployment.yaml
+    sed -i "s|\${RDS_ENDPOINT}|$RDS_ENDPOINT|g" ./ai/deployment.yaml
+    sed -i "s|\${REDIS_ENDPOINT}|$REDIS_ENDPOINT|g" ./ai/deployment.yaml
+else
+    echo "Error: no ./ai/deployment.yaml"
+fi
+
+# 5. FE sync
 if [ -f "./frontend/deployment.yaml" ]; then
+    sed -i "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" ./frontend/deployment.yaml
     sed -i "s|\${BE_API_URL}|$BE_API_URL|g" ./frontend/deployment.yaml
 fi
 
-# 4. Git push
-git add .
-git commit -m "chore: auto-deploy - infrastructure sync [$(date +'%Y-%m-%d %H:%M:%S')]"
-git push origin main
+# 6. Git push
+#git add .
+#git commit -m "chore: auto-deploy - infrastructure sync [$(date +'%Y-%m-%d %H:%M:%S')]"
+#git push origin main
 
 echo "------------------------------------------"
 echo "done!"
