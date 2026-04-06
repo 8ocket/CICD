4916e593a6460b369e69caf4a06f6221c7358690
hoombahh <hanwoolmeroomar@gmail.com>
2026-04-06 16:55:28 +0900

    chore: infrastructure sync [2026-04-06 16:55:28]

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,6 @@
 
 # 1. AWS info
 export ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
-export ECR_IMAGE_URL="${ACCOUNT_ID}.dkr.ecr.ap-northeast-2.amazonaws.com/mindlog-be:latest"
-
 export RDS_ENDPOINT=$(aws rds describe-db-instances \
   --db-instance-identifier rds-prod-8ocket \
   --query 'DBInstances[0].Endpoint.Address' --output text)
@@ -12,58 +10,45 @@ export REDIS_ENDPOINT=$(aws elasticache describe-replication-groups \
   --replication-group-id valkey-prod-8ocket \
   --query 'ReplicationGroups[0].NodeGroups[0].PrimaryEndpoint.Address' --output text)
 
-# BE lb
+# BE lb host
 BE_LB_HOST=$(kubectl get svc mindlog-be-service -n default -o jsonpath='{.status.loadBalancer.ingress[0].hostname}')
 export BE_API_URL="http://${BE_LB_HOST}:8080"
 
 echo "------------------------------------------"
-echo "   - ECR Image: $ECR_IMAGE_URL"
-echo "   - RDS Endpoint: $RDS_ENDPOINT"
+echo "   - Account ID: $ACCOUNT_ID"
+echo "   - RDS: $RDS_ENDPOINT"
 echo "   - Redis: $REDIS_ENDPOINT"
 echo "   - BE API: $BE_API_URL"
 echo "------------------------------------------"
 
-# 2. BE secret 
-kubectl delete secret mindlog-be-secret -n default --ignore-not-found
-kubectl create secret generic mindlog-be-secret -n default \
-  --from-literal=DB_USERNAME="mindlog" \
-  --from-literal=DB_PASSWORD='test1234' \
-  --from-literal=DB_NAME='mindlog_db' \
-  --from-literal=KT_JWT_SECRET='kt-cloud-8ocket-mindLog-jwt-secret-key' \
-  --from-literal=DB_DEV_HOST="$RDS_ENDPOINT" \
-  --from-literal=REDIS_DEV_HOST="$REDIS_ENDPOINT" \
-  --from-literal=REDIS_PASSWORD=''
+# 2. setup secret 
+chmod +x ./setup-secrets.sh
+./setup-secrets.sh
 
+# 3. Template -> YAML Sync
 
-# 3. BE sync
-if [ -f "./backend/deployment.yaml" ]; then
-    sed -i "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" ./backend/deployment.yaml
-    sed -i "s|\${RDS_ENDPOINT}|$RDS_ENDPOINT|g" ./backend/deployment.yaml
-    sed -i "s|\${REDIS_ENDPOINT}|$REDIS_ENDPOINT|g" ./backend/deployment.yaml
-else
-    echo "Error: no ./backend/deployment.yaml "
-    exit 1
-fi
+# BE Sync
+sed -e "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" \
+    -e "s|\${RDS_ENDPOINT}|$RDS_ENDPOINT|g" \
+    -e "s|\${REDIS_ENDPOINT}|$REDIS_ENDPOINT|g" ./backend/deployment.yaml.template > ./backend/deployment.yaml
 
-# 4. AI sync
-if [ -f "./ai/deployment.yaml" ]; then
-    sed -i "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" ./ai/deployment.yaml
-    sed -i "s|\${RDS_ENDPOINT}|$RDS_ENDPOINT|g" ./ai/deployment.yaml
-    sed -i "s|\${REDIS_ENDPOINT}|$REDIS_ENDPOINT|g" ./ai/deployment.yaml
-else
-    echo "Error: no ./ai/deployment.yaml"
-fi
+# AI Sync
+sed -e "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" \
+    -e "s|\${RDS_ENDPOINT}|$RDS_ENDPOINT|g" \
+    -e "s|\${REDIS_ENDPOINT}|$REDIS_ENDPOINT|g" ./ai/deployment.yaml.template > ./ai/deployment.yaml
 
-# 5. FE sync
-if [ -f "./frontend/deployment.yaml" ]; then
-    sed -i "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" ./frontend/deployment.yaml
-    sed -i "s|\${BE_API_URL}|$BE_API_URL|g" ./frontend/deployment.yaml
-fi
+# FE Sync
+sed -e "s|\${ACCOUNT_ID}|$ACCOUNT_ID|g" \
+    -e "s|\${BE_API_URL}|$BE_API_URL|g" ./frontend/deployment.yaml.template > ./frontend/deployment.yaml
 
-# 6. Git push
-#git add .
-#git commit -m "chore: auto-deploy - infrastructure sync [$(date +'%Y-%m-%d %H:%M:%S')]"
-#git push origin main
+# 4. Git Push 
+git add .
+git commit -m "chore: infrastructure sync [$(date +'%Y-%m-%d %H:%M:%S')]"
+git push origin main
+
+# 5. Cleanup 
+kubectl delete pods --all --field-selector status.phase=Pending
 
 echo "------------------------------------------"
-echo "done!"
+echo "Git Push Done! ArgoCD will sync automatically."
+echo "------------------------------------------"
